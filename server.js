f199a9e4f5b9d8d91ad69e4adffc4394b6893de1
Djeeb <ojeanbaptiste75@gmail.com>
2023-11-30 17:33:48 +0100

    config: Mise en place du serveur Express

---
+++ server.js
@@ -0,0 +1,27 @@
+/***************************************/
+/*** Fichier de lancement du serveur ***/
+/***************************************/
+require('dotenv').config();
+// Importation du module path
+const path = require('path');
+// Importation du module express
+const express = require('express');
+
+// Définition du port d'écoute du serveur
+const PORT = process.env.PORT ?? 5000; 
+
+// Création de l'application express
+const app = express();
+
+// Autorise la réception de données au format (Content-type) JSON
+app.use(express.json());
+
+// Définition du dossier public
+app.use(express.static('public'));
+// Autorise la réception de données au format (Content-type) x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// je lance l'écoute de mon serveur
+app.listen(PORT,()=>{
+  console.log(`Serveur démarré http://localhost:${PORT}`);
+});
