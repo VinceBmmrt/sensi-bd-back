733a47ab739b605bf8bf4c35da7f5b3d81871fe8
julienserbielle <julien.serbielle@oclock.school>
2023-12-08 15:31:41 +0100

    test: mise en place de la gestion des erreurs dans les routes

--- app/controllers/helpers/errorHandler.js
+++ app/controllers/helpers/errorHandler.js
@@ -1,6 +1,8 @@
 const debug = require('debug')('sensibd:errorHandler');
 const ApiError = require('../../errors/ApiError');
 
+// Middleware de Express pour la gestion d'erreur
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (error, _, res, next) => {
   debug('errorHandler', error);
   if (error instanceof ApiError) {
--- app/routers/user.js
+++ app/routers/user.js
@@ -1,19 +1,34 @@
-// Importation du controleur userController
-const express = require('express');
-const { userController } = require('../controllers');
 // Importation du module express
+const express = require('express');
+// Importation du controleur userController
+const { userController } = require('../controllers');
+// Importation de controllerHandler
+const controllerHandler = require('../controllers/helpers/controllerHandler');
+// Importation de ApiError
+const ApiError = require('../errors/ApiError');
+// Importation de errorHandler
+const errorHandler = require('../controllers/helpers/errorHandler');
+
 // Création du router
 const router = express.Router();
 
 // Définition des routes
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', userController.addUser);
+router.get('/', controllerHandler(userController.getAllUsers));
+router.get('/:id', controllerHandler(userController.getUserById));
+router.post('/', controllerHandler(userController.addUser));
 
-router.patch('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.patch('/:id', controllerHandler(userController.updateUser));
+router.delete('/:id', controllerHandler(userController.deleteUser));
 
-router.patch('/:id/address', userController.updateUserAddress);
+router.patch('/:id/address', controllerHandler(userController.updateUserAddress));
+
+// Middleware pour gérer les routes qui ne correspondent à aucune route définie ci-dessus.
+router.use(() => {
+  throw new ApiError('Resource not found', 404);
+});
+
+// Middleware pour la gestion des erreurs.
+router.use(errorHandler);
 
 // Exportation du router
 module.exports = router;
