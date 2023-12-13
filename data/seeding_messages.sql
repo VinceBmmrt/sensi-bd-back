7d9030e3c6ebf8c95cd492f3a7af3f402ccb66d9
julienserbielle <julien.serbielle@oclock.school>
2023-12-13 15:35:12 +0100

    feat: refonte d'un seeding pour la messagerie

--- data/seeding_messages.sql
+++ data/seeding_messages.sql
@@ -2,8 +2,10 @@ BEGIN;
 
 -- Insertion de données dans les messages
 INSERT INTO "message" ("sender_id", "receiver_id", "post_id", "content") VALUES
-(1, 2, 1, 'Bonjour, je suis intéressé par votre annonce'),
-(2, 1, 1, 'Merci pour votre message, je suis disponible pour l''échanger'),
-(3, 2, 1, 'Bonjour, je suis intéressé par votre annonce moi aussi !');
+(9, 10, 1, 'Bonjour, je suis intéressé par votre annonce'),
+(10, 9, 1, 'Merci pour votre message, je suis disponible pour l''échanger'),
+(9, 10, 1, 'Êtes vous dispo ce samedi matin ?'),
+(11, 10, 1, 'Bonjour, je suis intéressé par votre annonce moi aussi !'),
+(10, 11, 1, 'Ah désolé on viens de me contacter pour cette annonce précise'),
 
 COMMIT;
