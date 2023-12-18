f7add5e61d0ff32cecad262f4eb8b90fd1d52111
Djeeb <ojeanbaptiste75@gmail.com>
2023-12-18 15:54:19 +0100

    refactor: datamapper des conditions

--- app/datamappers/condition.js
+++ app/datamappers/condition.js
@@ -6,8 +6,8 @@ const pool = require('./pool');
 // Initialisation à la connexion à la base de données
 const conditionDatamapper = {
   /**
-   * Méthode: récupérer toutes les conditions de la base de données
-   * @returns retourne tout les conditions
+   * Méthode: récupérer toutes les conditions (états des ouvrages) de la base de données
+   * @returns retourne tous les états des ouvrages
    */
   async findAll() {
     const sqlQuery = 'SELECT * FROM "condition"';
@@ -15,9 +15,9 @@ const conditionDatamapper = {
     return results.rows;
   },
   /**
-   * Méthode: récupérer une condition de la BDD par son ID
-   * @param {number} id id de la condition
-   * @returns retourne une condition
+   * Méthode: récupérer une condition (état de l'ouvrage) de la BDD par son ID
+   * @param {number} id id de l'état de l'ouvrage
+   * @returns retourne un état de l'ouvrage par son id
    */
   async findById(id) {
     const sqlQuery = 'SELECT * FROM "condition" WHERE id = $1';
