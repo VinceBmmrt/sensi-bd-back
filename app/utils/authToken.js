29007fba7aa03934cc4fa6b7a4e323d0f369882c
julienserbielle <julien.serbielle@oclock.school>
2023-12-18 15:25:10 +0100

    refacto: Ajout de commentaire sur les middlewares JWT

--- app/utils/authToken.js
+++ app/utils/authToken.js
@@ -5,8 +5,11 @@ const JWT_SIGN_SECRET = '5sdza45gffzd47akbijfldj12djzjsdl554df5f5z5jfjfj4f5fz45f
 
 module.exports = (req, res, next) => {
   try {
+    // Récupération du token dans le header du req, en séparant le Bearer du token
     const token = req.headers.authorization.split(' ')[1];
+    // Utilisation de la méthode verify de JWT pour vérifier la validité du token
     const decodedToken = jwt.verify(token, JWT_SIGN_SECRET);
+    // Récupération du userId depuis le header du token pour l'utiliser dans des méthodes
     const { userId } = decodedToken;
     req.auth = {
       userId,
--- app/utils/genToken.js
+++ app/utils/genToken.js
@@ -4,14 +4,23 @@ const jwt = require('jsonwebtoken');
 const JWT_SIGN_SECRET = '5sdza45gffzd47akbijfldj12djzjsdl554df5f5z5jfjfj4f5fz45fzjnzdkjdzf22z7zkjdfizchlkdfkj45zcjcznjfj';
 
 module.exports = {
+  /**
+   * Fonction de génération de token JWT
+   * @param {Object} userData les infos d'un utilisateur enregistré
+   * @returns un token JWT valide
+   */
   generateTokenForUser(userData) {
+    // Utilisation de la méthode sign de JWT pour créer un nouveau token
     return jwt.sign(
+      // Assignation d'informations lié à l'utilisateur dans son token
       {
         userId: userData.id,
         role: userData.role_id,
         score: userData.score,
       },
+      // Clé secrète de signature de token
       JWT_SIGN_SECRET,
+      // Assignation d'un durée d'expiration du token
       {
         expiresIn: '1h',
       },
