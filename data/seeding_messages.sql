55d872bd56ed5c1c3de85057f1411ecacb9f2eb2
julienserbielle <julien.serbielle@oclock.school>
2023-12-19 17:32:45 +0100

    fix: ajout de commentaires au seeding_messages.sql

--- data/seeding_messages.sql
+++ data/seeding_messages.sql
@@ -1,6 +1,10 @@
 BEGIN;
 
 -- Insertion de données dans les messages
+-- ** ATTENTION **
+-- L'ajout de ce seeding doit être fait après le seeding.sql et après avoir rajouté à la main 3 utilisateurs en BDD
+-- Pour que leur mot de passe puisse être hashé dans la méthode addUser du controller des users
+
 INSERT INTO "message" ("sender_id", "receiver_id", "post_id", "content") VALUES
 (9, 10, 1, 'Bonjour, je suis intéressé par votre annonce'),
 (10, 9, 1, 'Merci pour votre message, je suis disponible pour l''échanger'),
