14c77f9f57bfc5556e26594f34816a5b29cb0dda
Djeeb <ojeanbaptiste75@gmail.com>
2023-12-26 14:23:04 +0100

    config: documentation pour la route Get du router Condition

--- app/routers/condition.js
+++ app/routers/condition.js
@@ -6,7 +6,33 @@ const { conditionController } = require('../controllers');
 const router = express.Router();
 
 // Définition des routes
+/**
+ * Condition types
+ * @typedef {object} Condition
+ * @property {number} id - id de la condition
+ * @property {string} name - nom de la condition
+ */
+
+/**
+ * GET /conditions
+ * @summary retourne une liste de conditions
+ * @return {Condition} 200 - Success response
+ */
 router.get('/', conditionController.getAllConditions);
+
+/**
+ * Condition types
+ * @typedef {object} Condition
+ * @property {number} id - id de la condition
+ * @property {string} name - nom de la condition
+ */
+
+/**
+ * GET /conditions/{id}
+ * @summary retourne une condition en fonction d'un id
+ * @param {number} id.path.required - l'id de la condition
+ * @return {object} 200 - Success response
+ */
 router.get('/:id', conditionController.getConditionById);
 
 // Exportation du router
