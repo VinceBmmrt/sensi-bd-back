8e5b65b6d4ec54568557c8444d425c166cf3c54a
Djeeb <ojeanbaptiste75@gmail.com>
2023-12-26 14:17:09 +0100

    config: documentation pour la route Get du router Audience

--- app/routers/audience.js
+++ app/routers/audience.js
@@ -6,6 +6,18 @@ const { audienceController } = require('../controllers');
 const router = express.Router();
 
 // Définition des routes
+/**
+ * Audience types
+ * @typedef {object} Audience
+ * @property {number} id - id de l'audience
+ * @property {string} name - nom de l'audience
+ */
+
+/**
+ * GET /audiences
+ * @summary retourne une liste d'audiences
+ * @return {Audience} 200 - Success response
+ */
 router.get('/', audienceController.getAllAudiences);
 
 // Exportation du router
