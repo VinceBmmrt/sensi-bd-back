e5671e38c1d7ec5fcdd7301262ecf37e2c703822
Djeeb <ojeanbaptiste75@gmail.com>
2023-12-26 16:21:05 +0100

    config: documentation pour les routes GET et POST des routeurs Message et Conversation

--- app/routers/message.js
+++ app/routers/message.js
@@ -14,8 +14,40 @@ const router = express.Router();
 const auth = require('../utils/authToken');
 
 // Définition des routes
+/**
+ * Message types
+ * @typedef {object} Message
+ * @property {number} id - id du message
+ * @property {string} content - contenu du message
+ * @property {number} post_id - id de l'annonce
+ * @property {number} user_id - id de l'utilisateur
+ */
+
+/**
+ * GET /messages
+ * @summary retourne une liste de messages
+ * @param {number} postId.path.required - id de l'annonce
+ * @param {number} userId.path.required - id de l'utilisateur
+ * @return {Message} 200 - Success response
+ */
 router.get('/:postId/:userId', auth, controllerHandler(messageController.getMessagesByPostAndUser));
+
+/**
+ * POST /messages
+ * @summary ajoute un message à une annonce
+ * @param {number} postId.path.required - id de l'annonce
+ * @param {number} userId.path.required - id de l'utilisateur
+ * @param {string} content.body.required - contenu du message
+ * @return {Message} 200 - Success response
+ */
 router.post('/:postId/:userId', auth, controllerHandler(messageController.addMessage));
+
+/**
+ * GET /messages/conversations
+ * @summary retourne toutes les conversations d'un utilisateur
+ * @security JWT
+ * @return {conversation} 200 - Liste des conversations de l'utilisateur authentifié
+ */
 router.get('/conversations', auth, controllerHandler(messageController.getConversations));
 
 // Middleware pour gérer les routes qui ne correspondent à aucune route définie ci-dessus.
